57039a40126663c7a27bfcccf5972884e27f09a8
Michael Bahr <michael@bahr.dev>
2021-12-06 12:55:52 +0100

    Add route to create app

---
+++ src/functions/api/createApp/handler.ts
@@ -0,0 +1,40 @@
+import 'source-map-support/register';
+import * as DynamoDB from 'aws-sdk/clients/dynamodb';
+import {APIGatewayProxyEventBase} from "aws-lambda";
+import {metricScope} from "aws-embedded-metrics";
+import {v4 as uuid} from 'uuid';
+import {App} from "../../types";
+
+const ddb = new DynamoDB.DocumentClient();
+
+const {OWNERS_TABLE} = process.env;
+
+export const main = metricScope(metrics => async (event: APIGatewayProxyEventBase<any>) => {
+
+    const owner = event.requestContext?.authorizer?.claims['cognito:username'];
+    const {name} = JSON.parse(event.body);
+
+    const apiKey = uuid();
+    const id = uuid();
+
+    const app: App = {
+        owner,
+        name,
+        id,
+        apiKey,
+    };
+
+    await ddb.put({
+        TableName: OWNERS_TABLE,
+        Item: app,
+    }).promise();
+
+    metrics.setNamespace("DEV/ServerlessScheduler/CreateApp");
+    metrics.setProperty("Owner", owner);
+    metrics.setProperty("App", id);
+
+    return {
+        statusCode: 200,
+        body: app,
+    }
+});
---
+++ src/functions/api/createApp/index.ts
@@ -0,0 +1,27 @@
+export default {
+  handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
+  events: [{
+    http: {
+      method: 'PUT',
+      path: '/owner/app',
+      authorizer: {
+        type: 'COGNITO_USER_POOLS',
+        authorizerId: { Ref: 'MyApiGatewayAuthorizer' },
+      },
+      cors: true,
+    }
+  }],
+  environment: {
+    OWNERS_TABLE: {Ref: 'OwnersTable'},
+  },
+  iamRoleStatements: [
+    {
+      Effect: 'Allow',
+      Action: ['dynamodb:PutItem'],
+      Resource: {'Fn::GetAtt': ['OwnersTable', 'Arn']}
+    },
+  ],
+  tags: {
+    resource: 'serverless-scheduler-api-creatApp',
+  },
+}
