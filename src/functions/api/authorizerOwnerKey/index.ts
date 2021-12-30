df5c1c86cfdb7e1d0185c269dfe6927d55017b3a
Michael Bahr <michael@bahr.dev>
2021-12-30 16:22:36 +0100

    Fix authorizer table access

--- src/functions/api/authorizerOwnerKey/index.ts
+++ src/functions/api/authorizerOwnerKey/index.ts
@@ -1,13 +1,13 @@
 export default {
   handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
   environment: {
-    OWNERS_TABLE: {Ref: 'OwnersTable'},
+    API_KEY_TABLE: {Ref: 'ApiKeyTable'},
   },
   iamRoleStatements: [
     {
       Effect: 'Allow',
-      Action: ['dynamodb:Query'],
-      Resource: {'Fn::Join': [ '/', [{ 'Fn::GetAtt': ['OwnersTable', 'Arn' ] }, 'index', 'apiKeyIndex' ]]}
+      Action: ['dynamodb:GetItem'],
+      Resource: { 'Fn::GetAtt': ['ApiKeyTable', 'Arn' ] },
     },
   ],
   tags: {
