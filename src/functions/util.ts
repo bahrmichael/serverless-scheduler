2df19e94d48bf77a6700ed52ec49318b2859be16
Michael Bahr <michael@bahr.dev>
2022-01-06 21:26:27 +0100

    Delay must not be negative

--- src/functions/util.ts
+++ src/functions/util.ts
@@ -1,4 +1,6 @@
 export function calculateDelay(sendAt: string): number {
     // Don't deliver messages before they should arrive. Opt for up to a second later instead. That's why we ceil here.
-    return Math.ceil((new Date(sendAt).getTime() - new Date().getTime()) / 1_000);
+    const delay = Math.ceil((new Date(sendAt).getTime() - new Date().getTime()) / 1_000);
+    // Delay must not be negative
+    return Math.max(delay, 0);
 }
