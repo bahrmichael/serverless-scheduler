349ca2ac0d1c3887c6ee9ddee4768e51e978dfdb
Michael Bahr <michael@bahr.dev>
2022-01-10 18:37:59 +0100

    Use dynamic payload

--- src/functions/types.ts
+++ src/functions/types.ts
@@ -45,7 +45,7 @@ export interface Message {
     messageId: string;
     sendAt: string;
     status: MessageStatus;
-    payload: string;
+    payload: any;
     /*
     We use the GSI1 for loading READY items. We populate it when inserting items, and clear the fields when we queue them.
     The PK is <owner>#<status> and the SK is <date>#<ULID>.
