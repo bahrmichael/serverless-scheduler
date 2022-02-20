80e0bbd70a1cb40ee5552bf3cc9ffa8dcc0c5975
Michael Bahr <michael@bahr.dev>
2022-02-20 17:59:42 +0100

    Don't let sls generate an api key

--- serverless.ts
+++ serverless.ts
@@ -37,7 +37,7 @@ const serverlessConfiguration: AWS = {
         apiGateway: {
             minimumCompressionSize: 1024,
             shouldStartNameWithService: true,
-            apiKeys: ["API_KEY"],
+            // apiKeys: ["API_KEY"],
             apiKeySourceType: 'AUTHORIZER'
         },
         environment: {
